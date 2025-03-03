5377f1f3c08695c6bf0f219482b493d07f7a3716
RyanMoulday <RyanMoulday@outlook.com>
2025-03-02 23:27:12 -0500

    Only run on physical cores

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ make release
 echo Running matrix-free-kokkos on the GPU
 ./matrix-free-kokkos >> out.txt
 echo Running matrix-based-petsc on the Host with $(nproc) cores
-mpirun -n $(nproc) --use-hwthread-cpus matrix-based-petsc >> out.txt
+mpirun -n $(grep -m 1 "cpu cores" /proc/cpuinfo | tr -d -c 0-9) matrix-based-petsc >> out.txt
 echo Running matrix-based-trilinos on the Host with $(nproc) cores
-mpirun -n $(nproc) --use-hwthread-cpus matrix-based-trilinos >> out.txt
+mpirun -n $(grep -m 1 "cpu cores" /proc/cpuinfo | tr -d -c 0-9) matrix-based-trilinos >> out.txt
