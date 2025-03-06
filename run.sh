0f5a8a350be0466d3ba582f1fd0d050bd6e665de
RyanMoulday <RyanMoulday@outlook.com>
2025-03-06 15:11:07 -0500

    Add matrix-free-cpu

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ make release
 echo Running matrix-free-kokkos on the GPU
 ./matrix-free-kokkos 6 >> out.txt
 ./matrix-free-kokkos 7 >> out.txt
+echo Running matrix-free-cpu on the CPU
+mpirun -n $(grep -m 1 "cpu cores" /proc/cpuinfo | tr -d -c 0-9) ./matrix-free-cpu 6 >> out.txt
+mpirun -n $(grep -m 1 "cpu cores" /proc/cpuinfo | tr -d -c 0-9) ./matrix-free-cpu 7 >> out.txt
 echo Running matrix-based-petsc on the Host with $(nproc) cores
 mpirun -n $(grep -m 1 "cpu cores" /proc/cpuinfo | tr -d -c 0-9) matrix-based-petsc 6 >> out.txt
 mpirun -n $(grep -m 1 "cpu cores" /proc/cpuinfo | tr -d -c 0-9) matrix-based-petsc 7 >> out.txt
